44f9009d5213b9114fe05de74ba8d23ee40ee05b
enia <elvio.vieira95@gmail.com>
2023-04-13 18:52:55 -0300

    CandidatoService

--- src/main/java/br/com/vivo/desafio/api/controller/CandidatoController.java
+++ src/main/java/br/com/vivo/desafio/api/controller/CandidatoController.java
@@ -1,11 +1,17 @@
 package br.com.vivo.desafio.api.controller;
 
 import br.com.vivo.desafio.api.dto.DadosCandidatos;
+import br.com.vivo.desafio.api.service.CandidateService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
 @RestController
 //anotação usada para api rest
 //Para que o Spring reconheça a classe como um Controller
@@ -13,11 +19,24 @@ import org.springframework.web.bind.annotation.RestController;
 //qual é o url que o controller vai responder, quando chegar uma requisição
 public class CandidatoController {
 
+    @Autowired
+    private CandidateService candidateService;
+
     @PostMapping
     //chamando requisição Post no insomnia
 
-    public DadosCandidatos cadastrar(@RequestBody @Valid DadosCandidatos dados){
-    System.out.println(dados);
-    return dados;
+    public DadosCandidatos cadastrar(@RequestBody @Valid DadosCandidatos dados) {
+        System.out.println(dados);
+        return dados;
+    }
+
+    @PostMapping("/criarcandidato")
+    public ResponseEntity<DadosCandidatos> criarCandidato(@RequestBody String json) {
+        try {
+            DadosCandidatos candidatos = candidateService.criarCandidato(json);
+            return ResponseEntity.ok(candidatos);
+        } catch (IllegalArgumentException | IOException e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
--- src/main/java/br/com/vivo/desafio/api/service/CandidateService.java
+++ src/main/java/br/com/vivo/desafio/api/service/CandidateService.java
@@ -1,13 +1,28 @@
 package br.com.vivo.desafio.api.service;
 
 
+import br.com.vivo.desafio.api.dto.DadosCandidatos;
 import br.com.vivo.desafio.api.dto.SkillsDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.List;
 
 
 @Service
 public class CandidateService {
-    public List<SkillsDTO>
+    public DadosCandidatos criarCandidato(String json) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        DadosCandidatos candidatos = objectMapper.readValue(json, DadosCandidatos.class);
+
+        for (SkillsDTO skills : candidatos.getHabilidade()) {
+            if (skills.getPontuacao() < 0 || skills.getPontuacao() > 10) {
+                throw new IllegalArgumentException("A nota deve estar entre 0 e 10.");
+            }
+        }
+
+        return candidatos;
+    }
+
 }
